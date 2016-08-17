7c514f837e028ee48c925ef9b21ccc062e55535e
MichielAchten <michiel.achten@gmail.com>
2016-08-17 14:21:07 +0200

    OefeningKlantenEnHunRekeningen1

--- Taken/Program.cs
+++ Taken/Program.cs
@@ -10,6 +10,29 @@ namespace Taken
     {
         static void Main(string[] args)
         {
+            List<Klant> klanten;
+            using (var entities = new BankEntities())
+            {
+                var query = from klant in entities.Klanten.Include("Rekeningen")
+                            orderby klant.Voornaam
+                            select klant;
+                klanten = query.ToList();
+            }
+            foreach (var eenKlant in klanten)
+            {
+                decimal totaal = Decimal.Zero;
+                Console.WriteLine(eenKlant.Voornaam);
+                //if (eenKlant.Rekeningen != null)
+                //{
+                    foreach (var eenRekening in eenKlant.Rekeningen)
+                    {
+                        Console.WriteLine("{0}: {1}", eenRekening.RekeningNr, eenRekening.Saldo);
+                        totaal += eenRekening.Saldo;
+                    }
+                //}
+                Console.WriteLine("Totaal: {0}", totaal);
+                Console.WriteLine();
+            }
         }
     }
 }
