acced88db74029f64983f8f73d2a0e387d657792
MichielAchten <michiel.achten@gmail.com>
2016-08-18 11:53:17 +0200

    OefeningStorten1

--- Taken/Program.cs
+++ Taken/Program.cs
@@ -10,39 +10,74 @@ namespace Taken
     {
         static void Main(string[] args)
         {
+
+
+            //1.4 storten
+            //using (var entities = new BankEntities())
+            //{
+            //    Console.Write("Rekeningnummer: ");
+            //    string rekeningNr = Console.ReadLine();
+            //    var rekening = entities.Rekeningen.Find(rekeningNr);
+            //    if (rekening != null)
+            //    {
+            //        Console.Write("Bedrag: ");
+            //        decimal bedrag;
+            //        if (decimal.TryParse(Console.ReadLine(), out bedrag))
+            //        {
+            //            if (bedrag > 0m)
+            //            {
+            //                rekening.Storten(bedrag);
+            //                entities.SaveChanges();
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine("Tik een positief getal");
+            //            }
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Tik een getal");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Rekening niet gevonden");
+            //    }
+            //}
+            
             //1.3 zichtrekening toevoegen(voorbeeldoplossing)
-            using (var entities = new BankEntities())
-            {
-                var query = from klant in entities.Klanten
-                            orderby klant.Voornaam
-                            select klant;
-                foreach (var klant in query)
-                {
-                    Console.WriteLine("{0}: {1}", klant.KlantNr, klant.Voornaam);
-                }
-                try
-                {
-                    Console.Write("KlantNr: ");
-                    var klantNr = int.Parse(Console.ReadLine());
-                    var klant = entities.Klanten.Find(klantNr);
-                    if (klant == null)
-                    {
-                        Console.WriteLine("Klant niet gevonden");
-                    }
-                    else
-                    {
-                        Console.Write("RekeningNr: ");
-                        var rekeningNr = Console.ReadLine();
-                        var rekening = new Rekening { RekeningNr = rekeningNr, Soort = "Z" };
-                        klant.Rekeningen.Add(rekening);
-                        entities.SaveChanges();
-                    }
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Tik een getal");
-                }
-            }
+            //using (var entities = new BankEntities())
+            //{
+            //    var query = from klant in entities.Klanten
+            //                orderby klant.Voornaam
+            //                select klant;
+            //    foreach (var klant in query)
+            //    {
+            //        Console.WriteLine("{0}: {1}", klant.KlantNr, klant.Voornaam);
+            //    }
+            //    try
+            //    {
+            //        Console.Write("KlantNr: ");
+            //        var klantNr = int.Parse(Console.ReadLine());
+            //        var klant = entities.Klanten.Find(klantNr);
+            //        if (klant == null)
+            //        {
+            //            Console.WriteLine("Klant niet gevonden");
+            //        }
+            //        else
+            //        {
+            //            Console.Write("RekeningNr: ");
+            //            var rekeningNr = Console.ReadLine();
+            //            var rekening = new Rekening { RekeningNr = rekeningNr, Soort = "Z" };
+            //            klant.Rekeningen.Add(rekening);
+            //            entities.SaveChanges();
+            //        }
+            //    }
+            //    catch (FormatException)
+            //    {
+            //        Console.WriteLine("Tik een getal");
+            //    }
+            //}
             
             //1.3 zichtrekening toevoegen(eigen oplossing)
             //List<Klant> klanten;
